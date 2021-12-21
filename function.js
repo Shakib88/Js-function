cf378d5071941bd3e64154d224f2da3ed86b4544
Shakib88 <shakibeeeuap28@gmail.com>
2021-12-21 21:50:24 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,94 @@
+// function sleep() {
+//     console.log("Jasim is sleeping")
+// }
+
+// sleep()
+// console.log('I am sleeping now')
+// sleep()
+// sleep()
+// sleep()
+// sleep()
+// sleep()
+
+//======================================================================
+
+// function sleep(names, time){
+//     console.log( names + " is sleeping now at " + time)
+// }
+
+// sleep('Jasim', "10 PM");
+// sleep('Rahim', "11 PM")
+
+
+//=====================================================================
+
+// function myFunction (a, b) {
+//     return a * b
+// }
+
+// let x = myFunction(4,5)
+// console.log(x)
+
+// myFunction(4,5)
+
+//=========================================================================
+
+// function myFunction (a, b)  {
+//         console.log("Hello")
+//         return a * b
+//     }
+    
+//     let x = myFunction(4,5)
+//     console.log(x)
+    
+//     myFunction(4,5)
+    
+//==============================================================================
+
+// function myFunction (a, b)  {
+//    console.log("hello")
+//    // autometic set return keyword 
+//    // If there no value there will be undefined variable
+// }
+
+// let x = myFunction(4,5)
+// console.log(x)
+
+//===================================================================================
+
+// function myFunction(){
+//     let carName = "BMW"
+//     console.log(carName)
+// }
+// myFunction()
+
+//====================================================================================
+
+//self invoking function/iife
+
+// (function (){
+//     console.log("hello")
+// })();
+
+//======================================================================================
+
+//Arrow function 
+// const x = function(x,y){
+//     return x * y
+// }
+
+// const x = (x,y) => x * y;
+
+// const x = function(x,y){
+//     console.log("hello")
+//     return x * y
+// }
+
+// const x = (x,y) => {
+//     console.log("hello")
+//     x * y;
+
+// }
+
+//====================================================================================
+
